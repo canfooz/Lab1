439d456601570b540f7bdef004328a01f70bde63
canfooz <canfooz2000@gmail.com>
2020-10-23 21:16:45 +0300

    Initial commit

---
+++ src/main/java/ArrayList.java
@@ -0,0 +1,56 @@
+/**
+ * @autor Chekmarev Andrey
+ * 3rd year, 7th group
+ * @version 1.1
+ *  ArrayList implementation class
+ */
+import java.util.Arrays;
+
+public class ArrayList<T> {
+    /** Last index field */
+    private int lastIndex = 0;
+
+    /** Initial lenght field */
+    private static final int SIZE = 2;
+
+    /** Array of size SIZE */
+    private Object[] array = new Object[SIZE];
+
+    /**
+     * @param newElement - some T element
+     * Increases the array size by 1
+     * Places the newElement in a new array element
+     */
+    public void add(T newElement) {
+        array = Arrays.copyOf(array, array.length + 1);
+        array[lastIndex++] = newElement;
+    }
+
+    /**
+     * Getter-method
+     * @param index - some int value
+     * @return T element returns an element with index of parameter
+     * @throws IndexOutOfBoundsException if no such index exists
+     */
+    T get(int index) {
+        if (index > lastIndex)
+            throw new IndexOutOfBoundsException();
+        return (T) array[index];
+    }
+
+    /**
+     * Getter-method
+     * @param index - some int value
+     *  Removes an element with the index specified in the parameter
+     * @throws IndexOutOfBoundsException if no such index exists
+     */
+    void remove(int index) {
+        if (index > lastIndex)
+            throw new IndexOutOfBoundsException();
+        else
+            System.arraycopy(array, index + 1, array, index, --lastIndex - index);
+        lastIndex--;
+    }
+}
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+/**
+ * @autor Chekmarev Andrey
+ * 3rd year, 7th group
+ * @version 1.1
+ *  Main class
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> arr = new ArrayList<> ();
+        arr.add ("one");
+        arr.add("three");
+        arr.add("five");
+        arr.add("seven");
+        arr.add("seven");
+        System.out.println(arr.get(0));
+        System.out.println(arr.get(2));
+        arr.remove(0);
+        System.out.println(arr.get(0));
+
+    }
+}
